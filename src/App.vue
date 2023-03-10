4a1a7f673897cd601bf05c51f060d3c1df36b859
brunaanjos <brunaanjosdaveiga@gmail.com>
2023-03-10 11:44:00 -0300

    100323

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,23 @@ export default {
       this.contador++
     },
     decrementarContador() {
+      if( this.contador > 0){
       this.contador--
+      }
+    },
+    reiniciarConatdor(){
+      this.contador = 0
     }
   },
   mounted() {
     console.log(`O valor inicial do contador é ${this.contador}.`)
   }
 }
+
 </script>
 
 <template>
+  <button @click="reiniciarConatdor">reiniciar</button>
   <button @click="incrementarContador">Incrementar</button>
   <button @click="decrementarContador">Decrementar</button>
   <p>Valor do contador é: {{ contador }}</p>
